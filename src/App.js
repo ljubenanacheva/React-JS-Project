1e7350212acdcdd834c9216e3d285a0bd391478f
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-03-28 15:31:15 +0300

    Added footer.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Footer } from "./Components/Footer/Footer.js";
 import { Header } from "./Components/Header/Header.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <div className="container">
+          <p id="footerP">Landmarks of Bulgaria</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
