b85873262332d566c180d209876cd639cd2442f0
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-03 16:44:44 +0300

    Edited style of home page.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,43 @@
+import Carousel from "react-bootstrap/Carousel";
+import styles from "./Home.module.css";
+
 export const Home = () => {
   return (
     <>
-      <section class="home-content">
-        <article class="home-content-text">
-          <h1>Explore Bulgaria</h1>
-          <h1 class="bold-home">Landmarks of Bulgaria</h1>
-          <p>You have seen an interesting place. Show it to us...</p>
+      <section className={styles["home-content"]}>
+        <article className={styles.homeContentText}>
+          <h1 className={styles.title}>Explore Bulgaria</h1>
+          <h1 className={styles.titleBold}>Landmarks of Bulgaria</h1>
+          <p className={styles.homeContentTextP}>
+            You have seen an interesting place. Show it to us...
+          </p>
         </article>
-        <article class="home-content-image">
+        <Carousel fade className={styles.carouselItem}>
+          <Carousel.Item>
+            <img
+              src="./images/phonemap.jpg"
+              alt="First slide"
+              className={styles.carouselImg}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="./images/Belogradchik.jpg"
+              alt="Second slide"
+              className={styles.carouselImg}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="./images/kaliakra.jpg"
+              alt="Third slide"
+              className={styles.carouselImg}
+            />
+          </Carousel.Item>
+        </Carousel>
+        {/* <article className={styles.homeContentImage}>
           <img src="./images/phonemap.jpg" alt="landmark photo" />
-        </article>
+        </article> */}
       </section>
     </>
   );
