3630e790aa31dc26787b11df5e369351a72c3cad
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-03 23:52:53 +0300

    Add authorization service file.

---
+++ src/services/authService.js
@@ -0,0 +1,18 @@
+import { requestFactory } from "./requester.js";
+
+const baseUrl = "http://localhost:3030/users";
+
+export const authServiceFactory = (token) => {
+  const request = requestFactory(token);
+  return {
+    login: (loginData) => {
+      return request.post(`${baseUrl}/login`, loginData);
+    },
+
+    register: (registerData) => {
+      return request.post(`${baseUrl}/register`, registerData);
+    },
+
+    logout: () => request.get(`${baseUrl}/logout`),
+  };
+};
