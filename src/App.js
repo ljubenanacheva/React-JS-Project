6d8ce5605203c72ad17301e57fffa32b593ceae5
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-03 04:08:13 +0300

    Add static regitser page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Create } from "./Components/Create/Create.js";
 import { Footer } from "./Components/Footer/Footer.js";
 import { Header } from "./Components/Header/Header.js";
 import { Home } from "./Components/Home/Home.js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Register } from "./Components/Register/Register.js";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Register />
+      <Create />
       <Footer />
     </>
   );
