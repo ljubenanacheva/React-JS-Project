de47c345a94a1f98c79b8a69315dc9b68a178875
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-03 14:10:43 +0300

    Footer coorection.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer>
       <div className="footer">
-        <p id="footerP">Landmarks of Bulgaria 2023©</p>
+        <p id="footerP">Landmarks of Bulgaria Lyubena Nacheva 2023©</p>
       </div>
     </footer>
   );
