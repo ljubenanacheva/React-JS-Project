027915da147426a96726c5fb45e80faad19b6a08
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-04 15:02:20 +0300

    Add Google Maps element to details page.

---
+++ src/Components/Details/Details.js
@@ -0,0 +1,13 @@
+export const Details = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11802.202065189955!2d27.754781679876636!3d42.309455488176106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a6cc14cc840ed5%3A0xefb61d9214cebeb!2sBeglik%20Tash!5e0!3m2!1sen!2sbg!4v1680551829009!5m2!1sen!2sbg"
+      width="600"
+      height="450"
+      style={{ border: "0" }}
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+};
