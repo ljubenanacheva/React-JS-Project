a44bf7f84cb784f60fe024beb58e1d17d9116cc3
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-04 15:07:46 +0300

    Make create and getAll functions in landmark service file.

---
+++ src/services/landmarkService.js
@@ -0,0 +1,24 @@
+import { requestFactory } from "./requester.js";
+
+const baseUrl = "http://localhost:3030/data/landmarks";
+
+export const landmarkServiceFactory = (token) => {
+  const request = requestFactory(token);
+  const getAll = async () => {
+    const result = await request.get(baseUrl);
+    const landmarks = Object.values(result);
+    return landmarks;
+  };
+  const create = async (landmarkData) => {
+    const result = await request.post(baseUrl, landmarkData);
+
+    return result;
+  };
+  return {
+    getAll,
+    //getOne,
+    create,
+    //edit,
+    //delete: deleteOne,
+  };
+};
