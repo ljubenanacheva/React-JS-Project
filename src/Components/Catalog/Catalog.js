069fe97a0d5e9a88a461f0f3d02a9f7c6d37d3d9
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-05 01:31:38 +0300

    Add dynamic catalog and landmark items.

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,27 +1,23 @@
 import styles from "./Catalog.module.css";
 import { Link } from "react-router-dom";
+import { LandmarkCard } from "./LandmarkCard.js";
+import { useLandmarkContext } from "../../contexts/LandmarkContext.js";
 
 export const Catalog = () => {
+  const { landmarks } = useLandmarkContext();
+
   return (
     <section>
       <h2 className={styles.catalogTitle}>Landmarks of Bulgaria</h2>
       <div className={styles.landMarkDashboard}>
-        <div className={styles.landmarkBoard}>
-          <article className="service-img">
-            <img className={styles.landmarkImage} src="./images/kaliakra.jpg" />
-          </article>
-          <h2 className={styles.landmakH2}>Kaliakra Cape</h2>
-          <h3 className={styles.landmarkH3}>Nature</h3>
-          <h5 className={styles.landmarkH5}>Kaliakra</h5>
-          <div className={styles.action}>
-            <Link className={styles.btn} to="#">
-              Details
-            </Link>
+        {landmarks.map((x) => (
+          <LandmarkCard key={x._id} {...x} />
+        ))}
+        {landmarks.length == 0 && (
+          <div>
+            <p className={styles.noLandmarks}>No landmarks in dashboard</p>
           </div>
-        </div>
-        {/*<div>
-          <p className={styles.noLandmarks}>No landmarks in dashboard</p>
-  </div>*/}
+        )}
       </div>
     </section>
   );
--- src/Components/Catalog/LandmarkCard.js
+++ src/Components/Catalog/LandmarkCard.js
@@ -1,16 +1,17 @@
-import style from "./Catalog.module.css";
+import styles from "./Catalog.module.css";
+import { Link } from "react-router-dom";
 
-export const LandmarkCard = () => {
+export const LandmarkCard = ({ _id, name, category, location, imageUrl }) => {
   return (
     <div className={styles.landmarkBoard}>
       <article className="service-img">
-        <img className={styles.landmarkImage} src="./images/kaliakra.jpg" />
+        <img className={styles.landmarkImage} src={imageUrl} />
       </article>
-      <h2 className={styles.landmakH2}>Kaliakra Cape</h2>
-      <h3 className={styles.landmarkH3}>Nature</h3>
-      <h5 className={styles.landmarkH5}>Kaliakra</h5>
+      <h2 className={styles.landmakH2}>{name}</h2>
+      <h3 className={styles.landmarkH3}>{category}</h3>
+      <h5 className={styles.landmarkH5}>{location}</h5>
       <div className={styles.action}>
-        <Link className={styles.btn} to="#">
+        <Link className={styles.btn} to={`/catalog/${_id}`}>
           Details
         </Link>
       </div>
